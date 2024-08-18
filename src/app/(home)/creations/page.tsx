c3bc6f9e256761a55e82a53962e99f2e37efacb4
cdcm <carloss_jt@hotmail.com>
2024-08-18 01:19:35 -0500

    [ADD] creation-card component

--- src/app/(home)/creations/page.tsx
+++ src/app/(home)/creations/page.tsx
@@ -1,11 +1,5 @@
-import Image from "next/image"
-import {
-  HeartIcon,
-  HeartFilledIcon,
-  ChatBubbleIcon,
-} from "@radix-ui/react-icons"
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
-import { Button } from "@/components/ui/button"
+import { formatDistanceToNow } from "date-fns"
+import CreationCard from "@/components/creation/creation-card"
 
 const creations = [
   {
@@ -13,22 +7,28 @@ const creations = [
     title: "Creation 1",
     creatorName: "John Doe",
     creatorImage: "/profile.jpg",
-    creationDate: "2022-01-01",
+    creationDate: formatDistanceToNow(new Date("2022-01-01"), {
+      addSuffix: true,
+    }),
     description: "Description 1",
     image: "/game.jpg",
     comments: "10",
     reactions: "10",
+    isReactionActive: false,
   },
   {
     id: "2",
     title: "Creation 2",
     creatorName: "John Doe",
     creatorImage: "/profile.jpg",
-    creationDate: "2022-01-01",
+    creationDate: formatDistanceToNow(new Date("2022-01-01"), {
+      addSuffix: true,
+    }),
     description: "Description 2",
     image: "/game.jpg",
     comments: "10",
     reactions: "10",
+    isReactionActive: true,
   },
 ]
 
@@ -36,56 +36,7 @@ const CreationsPage = () => {
   return (
     <div className="flex flex-col w-full text-sm pb-4">
       {creations.map((creation) => (
-        <div
-          key={creation.id}
-          className="w-[600px] mx-auto p-4 border-neutral-600 border-x border-b first:border-t"
-        >
-          <div className="flex gap-x-2 w-full">
-            <div className="h-[40px] w-[40px] rounded-full overflow-hidden relative">
-              <Image
-                alt={creation.creatorName}
-                src={creation.creatorImage}
-                layout="fill"
-                className="rounded-full object-cover"
-              />
-            </div>
-            <div className="flex-1">
-              <div className="space-y-2">
-                <div className="space-y-1">
-                  <div className="flex gap-x-2 items-center">
-                    <h1 className="font-semibold">{creation.title}</h1>
-                    <span className="text-neutral-400">
-                      {creation.creatorName}
-                    </span>
-                    <span className="text-neutral-400 leading-none h-[20px] align-middle">
-                      .
-                    </span>
-                    <span className="text-neutral-400">
-                      {creation.creationDate}
-                    </span>
-                  </div>
-                  <p>{creation.description}</p>
-                </div>
-                <div className="border rounded-lg overflow-hidden">
-                  <Image
-                    alt={creation.title}
-                    width={600}
-                    height={450}
-                    src={creation.image}
-                  />
-                </div>
-                <div className="flex justify-around w-full text-neutral-400 ">
-                  <Button variant={"ghost"}>
-                    <HeartIcon className="h-[18px] w-[18px] mr-1" /> <span className="text-sm">{creation.reactions}</span>
-                  </Button>
-                  <Button variant={"ghost"}>
-                    <ChatBubbleIcon className="h-[18px] w-[18px] mr-1" /> <span className="text-sm">{creation.comments}</span>
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <CreationCard key={creation.id} creation={creation}/>
       ))}
     </div>
   )
---
+++ src/components/creation/creation-card.tsx
@@ -0,0 +1,117 @@
+"use client"
+
+import Image from "next/image"
+import {
+  HeartIcon,
+  HeartFilledIcon,
+  ChatBubbleIcon,
+} from "@radix-ui/react-icons"
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { useState } from "react"
+
+interface CreationCardProps {
+  id: string
+  title: string
+  creatorName: string
+  creatorImage: string
+  creationDate: string
+  description: string
+  image: string
+  comments: string
+  reactions: string
+  isReactionActive: boolean
+}
+
+const CreationCard = ({ creation }: { creation: CreationCardProps }) => {
+  const [isReactionActive, setIsReactionActive] = useState(
+    creation.isReactionActive
+  )
+
+  return (
+    <div
+      key={creation.id}
+      className="w-[600px] mx-auto p-4 border-neutral-600 border-x border-b first:border-t"
+    >
+      <div className="flex gap-x-2 w-full">
+        <div className="h-[40px] w-[40px] rounded-full overflow-hidden relative">
+          <Image
+            alt={creation.creatorName}
+            src={creation.creatorImage}
+            layout="fill"
+            className="rounded-full object-cover"
+          />
+        </div>
+        <div className="flex-1">
+          <div className="space-y-2">
+            <div className="space-y-1">
+              <div className="flex gap-x-2 items-center text-neutral-400">
+                <h1 className="font-semibold text-dark-light">
+                  {creation.title}
+                </h1>
+                <span>{creation.creatorName}</span>
+                <span className="leading-none h-[20px] align-middle">.</span>
+                <span>{creation.creationDate}</span>
+              </div>
+              <p className="text-dark-dark">{creation.description}</p>
+            </div>
+            <div className="border rounded-lg overflow-hidden">
+              <Image
+                alt={creation.title}
+                width={600}
+                height={450}
+                src={creation.image}
+              />
+            </div>
+            <div className="flex justify-around w-full text-neutral-400 ">
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    variant={"ghost"}
+                    onClick={() => setIsReactionActive(!isReactionActive)}
+                    className={cn(
+                      "flex items-center",
+                      isReactionActive && "text-pink-600",
+                      "hover:text-pink-600"
+                    )}
+                  >
+                    {isReactionActive ? (
+                      <HeartFilledIcon className="h-[18px] w-[22px] mr-1" />
+                    ) : (
+                      <HeartIcon className="h-[18px] w-[22px] mr-1" />
+                    )}
+                    <span className="text-sm">{creation.reactions}</span>
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent className="bg-slate-700 opacity-90 text-slate-100">
+                  <p>Like</p>
+                </TooltipContent>
+              </Tooltip>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    variant={"ghost"}
+                    className="flex items-center hover:text-blue-400"
+                  >
+                    <ChatBubbleIcon className="h-[18px] w-[18px] mr-1" />{" "}
+                    <span className="text-sm">{creation.comments}</span>
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent className="bg-slate-700 opacity-90 text-slate-100">
+                  <p>Comment</p>
+                </TooltipContent>
+              </Tooltip>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CreationCard
