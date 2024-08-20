2e2b85d40c4fddb20eb9440c7def037a3760f7fd
cdcm <carloss_jt@hotmail.com>
2024-08-20 10:18:32 -0500

    [UPDATE] refactor creation-card

---
+++ src/components/creation/card-buttons.tsx
@@ -0,0 +1,65 @@
+"use client"
+
+import {
+  HeartIcon,
+  HeartFilledIcon,
+  ChatBubbleIcon,
+} from "@radix-ui/react-icons"
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { useState } from "react"
+import { CreationInList } from "@/types/creations"
+
+const CardButtons = ({ creation }: { creation: CreationInList }) => {
+  const [isReactionActive, setIsReactionActive] = useState(
+    creation.isReactionActive
+  )
+  return (
+    <div className="flex justify-around w-full text-neutral-400 ">
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant={"ghost"}
+            onClick={() => setIsReactionActive(!isReactionActive)}
+            className={cn(
+              "flex items-center",
+              isReactionActive && "text-pink-600",
+              "hover:text-pink-600"
+            )}
+          >
+            {isReactionActive ? (
+              <HeartFilledIcon className="h-[18px] w-[22px] mr-1" />
+            ) : (
+              <HeartIcon className="h-[18px] w-[22px] mr-1" />
+            )}
+            <span className="text-sm">{creation.reactions}</span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent className="bg-slate-700 opacity-90 text-slate-100">
+          <p>Like</p>
+        </TooltipContent>
+      </Tooltip>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant={"ghost"}
+            className="flex items-center hover:text-blue-400"
+          >
+            <ChatBubbleIcon className="h-[18px] w-[18px] mr-1" />{" "}
+            <span className="text-sm">{creation.comments}</span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent className="bg-slate-700 opacity-90 text-slate-100">
+          <p>Comment</p>
+        </TooltipContent>
+      </Tooltip>
+    </div>
+  )
+}
+
+export default CardButtons
--- src/components/creation/creation-card.tsx
+++ src/components/creation/creation-card.tsx
@@ -1,27 +1,10 @@
-"use client"
-
 import Image from "next/image"
-import {
-  HeartIcon,
-  HeartFilledIcon,
-  ChatBubbleIcon,
-} from "@radix-ui/react-icons"
-import { Button } from "@/components/ui/button"
-import { cn } from "@/lib/utils"
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/ui/tooltip"
-import { useState } from "react"
+
 import { formatDistanceToNow } from "date-fns"
 import { CreationInList } from "@/types/creations"
+import CardButtons from "./card-buttons"
 
 const CreationCard = ({ creation }: { creation: CreationInList }) => {
-  const [isReactionActive, setIsReactionActive] = useState(
-    creation.isReactionActive
-  )
-
   return (
     <div
       key={creation.id}
@@ -32,8 +15,8 @@ const CreationCard = ({ creation }: { creation: CreationInList }) => {
           <Image
             alt={creation.ownerName}
             src={creation.ownerImage}
-            layout="fill"
             className="rounded-full object-cover"
+            fill
           />
         </div>
         <div className="flex-1">
@@ -61,45 +44,7 @@ const CreationCard = ({ creation }: { creation: CreationInList }) => {
                 src={"/game.jpg"}
               />
             </div>
-            <div className="flex justify-around w-full text-neutral-400 ">
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Button
-                    variant={"ghost"}
-                    onClick={() => setIsReactionActive(!isReactionActive)}
-                    className={cn(
-                      "flex items-center",
-                      isReactionActive && "text-pink-600",
-                      "hover:text-pink-600"
-                    )}
-                  >
-                    {isReactionActive ? (
-                      <HeartFilledIcon className="h-[18px] w-[22px] mr-1" />
-                    ) : (
-                      <HeartIcon className="h-[18px] w-[22px] mr-1" />
-                    )}
-                    <span className="text-sm">{creation.reactions}</span>
-                  </Button>
-                </TooltipTrigger>
-                <TooltipContent className="bg-slate-700 opacity-90 text-slate-100">
-                  <p>Like</p>
-                </TooltipContent>
-              </Tooltip>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Button
-                    variant={"ghost"}
-                    className="flex items-center hover:text-blue-400"
-                  >
-                    <ChatBubbleIcon className="h-[18px] w-[18px] mr-1" />{" "}
-                    <span className="text-sm">{creation.comments}</span>
-                  </Button>
-                </TooltipTrigger>
-                <TooltipContent className="bg-slate-700 opacity-90 text-slate-100">
-                  <p>Comment</p>
-                </TooltipContent>
-              </Tooltip>
-            </div>
+            <CardButtons creation={creation} />
           </div>
         </div>
       </div>
