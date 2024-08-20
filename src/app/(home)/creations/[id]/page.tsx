c0b0da389c8864525fff627cc421569f65230027
cdcm <carloss_jt@hotmail.com>
2024-08-20 01:06:57 -0500

    [UPDATE] creation by id page

--- src/app/(home)/creations/[id]/page.tsx
+++ src/app/(home)/creations/[id]/page.tsx
@@ -1,9 +1,14 @@
+import { auth } from "@/auth"
+import CreationCard from "@/components/creation/creation-card"
+import { GetCreationById } from "@/services/creations"
 
-const CreationPage = ({ params }: { params: { id: string }}) => {
-  console.log(params.id)
+const CreationPage = async({ params }: { params: { id: string }}) => {
+  const session = await auth()
+  const data = await GetCreationById(session?.jwt, params.id)
+
   return (
     <div className="flex w-full flex-1 items-center justify-center">
-      hola mundo
+      <CreationCard creation={data}/>
     </div>
   )
 }
