5f0729ac5e3b190e8b6124e0388503863fac7517
cdcm <carloss_jt@hotmail.com>
2024-08-21 09:06:11 -0500

    [UPDATE] refactor creations page

--- src/app/(home)/creations/page.tsx
+++ src/app/(home)/creations/page.tsx
@@ -2,13 +2,20 @@ import { auth } from "@/auth"
 import { GetCreationsList } from "@/services/creations"
 import CreationCard from "@/components/creation/creation-card"
 
-const CreationsPage = async() => {
+const CreationsPage = async () => {
   const session = await auth()
-  const {data, pages} = await GetCreationsList(session?.jwt)
+  const { data, pages } = await GetCreationsList(session?.jwt)
   return (
-    <div className="flex flex-col w-fit mx-auto items-center text-sm pb-4 border-neutral-600 border-x">
+    <div className="flex flex-col w-fit mx-auto items-center text-sm border-neutral-600 border-x border-t">
       {data.map((creation) => (
-        <CreationCard key={creation.id} creation={creation} canRedirect/>
+        <CreationCard
+          key={creation.id}
+          creation={creation}
+          canRedirect
+          className={{
+            wrapper: "p-4",
+          }}
+        />
       ))}
     </div>
   )
