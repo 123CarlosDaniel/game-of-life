b5caab0e1be9945272beb6da61c13d3473e9d7b0
cdcm <carloss_jt@hotmail.com>
2024-08-21 19:30:38 -0500

    [UPDATE] endpoints and services new service for comments

---
+++ src/services/comments.ts
@@ -0,0 +1,43 @@
+"use server"
+
+import { endPoints } from "./endpoints"
+import {
+  createFetchError,
+  createNotFoundError,
+  createUnauthorizedError,
+} from "@/lib/errors"
+import { CommentForm } from "@/types/comment"
+import { CustomResponse } from "@/types/custom-error"
+
+export async function PostComment(
+  creationId: string,
+  data: CommentForm,
+  jwt: string | undefined
+) {
+  try {
+    const response = await fetch(endPoints.comments.create(creationId), {
+      headers: {
+        Authorization: jwt ? `Bearer ${jwt}` : "",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(data),
+    })
+    if (response.status === 404) {
+      throw createNotFoundError()
+    }
+    if (response.status === 401) {
+      throw createUnauthorizedError()
+    }
+    if (!response.ok) {
+      throw createFetchError(response.status)
+    }
+    return {
+      status: response.status,
+      body: await response.json(),
+    } as CustomResponse
+  } catch (error: unknown) {
+    const customError = error as CustomResponse
+    return customError
+  }
+}
--- src/services/endpoints.ts
+++ src/services/endpoints.ts
@@ -2,11 +2,16 @@ const API_URL = process.env.API_URL
 
 export const endPoints = {
   creations: {
-    all: (pageNumber: number, perPage: number, sortBy: string) => {
-      return `${API_URL}/creation/all?page-number=${pageNumber}&per-page=${perPage}&sort-by=${sortBy}`
+    all: (page: number, perPage: number, sortBy: string) => {
+      return `${API_URL}/creation/all?page=${page}&per_page=${perPage}&sort_by=${sortBy}`
     },
     byId: (id: string) => {
       return `${API_URL}/creation/${id}`
     }
   },
+  comments: {
+    create: (creationId: string) => {
+      return `${API_URL}/comment?creation_id=${creationId}`
+    }
+  }
 }
