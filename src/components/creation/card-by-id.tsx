c23462d557d6eec1a7b6af909bfac4239fd62b4d
cdcm <carloss_jt@hotmail.com>
2024-08-23 23:41:39 -0500

    [UPDATE] card-by-id component styles

--- src/components/creation/card-by-id.tsx
+++ src/components/creation/card-by-id.tsx
@@ -7,26 +7,17 @@ import Avatar from "../common/avatar"
 
 const CardById = ({ creation }: { creation: CreationInList }) => {
   return (
-    <div className="space-y-4">
+    <div className="space-y-4 text-[15px]">
       <div className="flex gap-x-2 w-full">
         <Avatar altSrc={creation.ownerName} src={creation.ownerImage} />
         <div className="flex flex-col text-neutral-400">
-          <div className="flex gap-x-2 items-center">
-            <h1 className="font-semibold text-dark-light first-letter:capitalize">
-              {creation.title}
-            </h1>
-            <span>·</span>
-            <span>
-              {formatDate(
-                new Date(creation.createdAt),
-                "MMMM dd, yyyy · hh:mm a"
-              )}
-            </span>
-          </div>
-          <span>{creation.ownerName}</span>
+          <h1 className="font-semibold text-dark-light tracking-wide">
+            {creation.title}
+          </h1>
+          <span className="tracking-wide">{creation.ownerName}</span>
         </div>
       </div>
-      <p className="text-dark-dark first-letter:capitalize">
+      <p className="text-dark-dark first-letter:capitalize text-[17px]">
         {creation.description}
       </p>
       <div className="border rounded-lg overflow-hidden">
@@ -37,6 +28,9 @@ const CardById = ({ creation }: { creation: CreationInList }) => {
           src={"/game.jpg"}
         />
       </div>
+      <span className="text-neutral-400 inline-block font-light">
+        {formatDate(new Date(creation.createdAt), "hh:mm a · MMM dd, yyyy")}
+      </span>
       <CardButtons creation={creation} />
     </div>
   )
