7e35429996320e656aa2a14dbf2ceaacd1f1036a
cdcm <carloss_jt@hotmail.com>
2024-09-02 16:21:18 -0500

    [UPDATE] home page

--- src/app/(principal)/home/page.tsx
+++ src/app/(principal)/home/page.tsx
@@ -1,23 +1,39 @@
 import { GetCreationsList } from "@/services/creations"
 import CreationCard from "@/components/creation/creation-card"
 import getSession from "@/lib/getSession"
+import Link from "next/link"
+import { ArrowLeftIcon } from "@radix-ui/react-icons"
 
 const CreationsPage = async () => {
   const session = await getSession()
   const { data, pages } = await GetCreationsList(session?.jwt)
   return (
-    <div className="flex flex-col w-fit mx-auto items-center text-sm border-neutral-600 border-x border-t">
-      {data.map((creation) => (
-        <CreationCard
-          key={creation.id}
-          creation={creation}
-          canRedirect
-          className={{
-            wrapper: "p-4",
-          }}
-        />
-      ))}
-    </div>
+    <>
+      {data.length === 0 ? (
+        <div className="p-4 space-y-4">
+          <div className="flex gap-x-8  items-center sticky z-30 top-0">
+            <Link href="/">
+              <ArrowLeftIcon className="h-6 w-6" />
+            </Link>
+            <span className="text-xl font-semibold tracking-wide">Back</span>
+          </div>
+          <p>No creations</p>
+        </div>
+      ) : (
+        <div className="flex flex-col w-fit mx-auto items-center text-sm border-neutral-600 border-x border-t">
+          {data.map((creation) => (
+            <CreationCard
+              key={creation.id}
+              creation={creation}
+              canRedirect
+              className={{
+                wrapper: "p-4",
+              }}
+            />
+          ))}
+        </div>
+      )}
+    </>
   )
 }
 
